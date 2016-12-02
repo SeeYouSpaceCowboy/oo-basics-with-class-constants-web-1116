201fc5eb6b6492cd786d82a4a9d3c780579b8600
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2016-12-01 22:46:10 -0500

    Completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,17 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    save_unique_brand(brand)
+  end
+
+  def save_unique_brand(new_brand)
+    if !BRANDS.include?(new_brand)
+      BRANDS << new_brand
+    end
   end
 
   def cobble
